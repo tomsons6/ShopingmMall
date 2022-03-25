21d3e0cba7832a56c74bf709d4786f0739994786
tomsons6 <toms.amsons@va.lv>
2022-03-25 14:19:02 +0200

    Touch control

--- Assets/ShoppingMall/Scripts/RotateCamera.cs
+++ Assets/ShoppingMall/Scripts/RotateCamera.cs
@@ -8,6 +8,20 @@ public class RotateCamera : MonoBehaviour
     float x;
     float y;
 
+    Vector3 FirstPoint;
+    Vector3 SecondPoint;
+    float xAngle;
+    float yAngle;
+    float xAngleTemp;
+    float yAngleTemp;
+
+
+    void Start()
+    {
+        xAngle = 0;
+        yAngle = 0;
+        this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +31,21 @@ public class RotateCamera : MonoBehaviour
             x = transform.rotation.eulerAngles.x;
             y = transform.rotation.eulerAngles.y;
             transform.rotation = Quaternion.Euler(x, y, 0);
+        }
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                FirstPoint = Input.GetTouch(0).position;
+                xAngleTemp = xAngle;
+                yAngleTemp = yAngle;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended){
+                SecondPoint = Input.GetTouch(0).position;
+                xAngle = xAngleTemp + (SecondPoint.x - FirstPoint.x) * 180 / Screen.width;
+                yAngle = yAngleTemp + (SecondPoint.y - FirstPoint.y) * 90 / Screen.height;
+                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+            }
         }
     }
 }
